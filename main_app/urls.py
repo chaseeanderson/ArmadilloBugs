d209fed3e301a9861d881a952bd6dff19de965e0
Harrison-Berek <hcberek@gmail.com>
2021-04-19 16:29:25 -0600

    Subscriber sign up

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.SubscriberCreate.as_view(), name='home'),
+    path('confirm/<int:subscriber_id>', views.confirm, name='confirm')
+    # path('index/', views.index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Subscriber
 
 # Create your views here.
+class SubscriberCreate(CreateView):
+    model = Subscriber
+    fields = '__all__'
+
+def confirm(request, subscriber_id):
+    subscriber = Subscriber.objects.get(id=subscriber_id)
+    return render('confirm.html', {'subscriber': subscriber})
